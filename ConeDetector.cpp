71f30214c0c54300f3d2371f31f63fc1603d5a47
James_Lozevski <101085956@student.swin.edu.au>
2021-05-10 17:10:14 +1000

    first commit

--- ConeDetector.cpp
+++ ConeDetector.cpp
@@ -1,2 +1,11 @@
+#include <iostream>
+#include <thread>
+#include <opencv2/opencv.hpp>
 
+using namespace cv;
+using namespace std;
 
+int main()
+{
+std::cout<< "Trying to push some data" << " ";
+}
