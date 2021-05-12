4e342fcd81fa5c0912b3ed81d85389b6e9704332
James_Lozevski <101085956@student.swin.edu.au>
2021-05-12 15:50:04 +1000

    OpenCV and Xtensor added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
-#include <opencv.hpp>
+#include "array"
+#include "xtensor/xtensor.hpp"
+#include "xtensor/xbuilder.hpp"
+#include "xtensor/xarray.hpp"
+#include "xtensor/xio.hpp"
+#include "xtensor/xfixed.hpp"
+#include "opencv2/opencv.hpp"
+#include "vector"
 
-
+using namespace cv;
 using namespace std;
 
 int main()
 {
-    cout << "Sample Text" << endl;
+
+xt::xarray<double> arr1
+{
+  {1.0, 2.0, 3.0},
+  {2.0, 5.0, 7.0},
+  {2.0, 5.0, 7.0}
+};
+
+xt::xarray<double> arr2
+  {5.0, 6.0, 7.0};
+
+xt::xarray<double> res = (arr1, 1) + arr2;
+cout << res;
+
+
 }
